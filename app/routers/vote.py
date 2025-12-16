888ffce127d9486581a29ab4289f623a89fea7c5
UllasDas004 <ullasdas110904@gmail.com>
2025-12-16 19:34:23 +0530

    Not valid post id validation check added

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -13,6 +13,9 @@ router = APIRouter(
 def vote(vote : schemas.Vote,db : Session = Depends(get_db),
          current_user : models.User = Depends(oauth2.get_current_user)):
     
+    get_post_id = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+    if not get_post_id:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with id{vote.post_id} not found")
     vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id,
                                                   models.Vote.user_id == current_user.id)
     found_vote = vote_query.first()
